c9c9eef55c5b97541059d2a3b68c53b0ed7672ab
avayedawadi <avaye.dawadi@gmail.com>
2020-12-27 15:28:51 -0500

    Fixed app.run

--- app.py
+++ app.py
@@ -103,6 +103,7 @@ def algorithm():
         return render_template("algorithm.html",user_image=filename) 
 
 
-
-if __name__ == "__main__":
-    app.run(debug=True)
+port = int(os.environ.get('PORT', 5000))
+app.run(host='0.0.0.0', port=port, debug=True)
+#if __name__ == "__main__":
+#    app.run(debug=True)
